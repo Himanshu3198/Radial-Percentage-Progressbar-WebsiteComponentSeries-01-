8995d0e5f315de9a29a038b6ff6cc8ee1f13b7d1
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2020-11-18 19:48:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log("this is javascript console")
+let options = {
+    startAngle: -1.515,
+    size: 150,
+    value: 0.85,
+    fill: {gradient: ['#a445b2', '#fa4299']}
+  }
+  $(".circle .bar").circleProgress(options).on('circle-animation-progress',
+  function(event, progress, stepValue){
+    $(this).parent().find("span").text(String(stepValue.toFixed(2).substr(2)) + "%");
+  });
+  $(".js .bar").circleProgress({
+    value: 0.70
+  });
+  $(".react .bar").circleProgress({
+    value:0.60
+  });
